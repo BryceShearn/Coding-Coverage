adc894904fb24074dfc4369ad26877d7beb6d69f
bryceshearn <bryce_shearn@yahoo.com>
2021-03-18 16:37:57 -0400

    Spring Boot MVC project created

---
+++ BootLearning/src/test/java/com/skilldistillery/learning/BootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
