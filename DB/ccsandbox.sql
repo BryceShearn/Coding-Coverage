bcc91bca257f69d4e6e25434f8fb60e737a63708
Thomas Webel <Thomaswebel@gmail.com>
2021-03-19 17:17:26 -0600

    Filled out User and tested table mapping

---
+++ DB/ccsandbox.sql
@@ -0,0 +1,26 @@
+show tables;
+
+desc code_concept;
+desc comment;
+desc comment_vote;
+desc language;
+desc post;
+desc post_has_language;
+desc post_vote;
+desc post_vote;
+desc resource;
+desc resource_has_language;
+desc user;
+desc user_has_roadmap;
+desc user_roadmap_task;
+
+
+
+
+
+
+
+
+
+
+
