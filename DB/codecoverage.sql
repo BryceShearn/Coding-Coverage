d31698990c095873e5368988474824f34ae0597f
Thomas Webel <Thomaswebel@gmail.com>
2021-03-20 14:04:05 -0600

    reverted resource to language relationship

--- DB/codecoverage.sql
+++ DB/codecoverage.sql
@@ -91,14 +91,7 @@ CREATE TABLE IF NOT EXISTS `resource` (
   `description` TEXT NOT NULL,
   `date_added` DATETIME NULL,
   `difficulty` VARCHAR(45) NULL,
-  `language_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_resource_language1_idx` (`language_id` ASC),
-  CONSTRAINT `fk_resource_language1`
-    FOREIGN KEY (`language_id`)
-    REFERENCES `language` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -319,6 +312,30 @@ CREATE TABLE IF NOT EXISTS `task_has_resource` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `resource_has_language`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `resource_has_language` ;
+
+CREATE TABLE IF NOT EXISTS `resource_has_language` (
+  `resource_id` INT NOT NULL,
+  `language_id` INT NOT NULL,
+  PRIMARY KEY (`resource_id`, `language_id`),
+  INDEX `fk_resource_has_language_language1_idx` (`language_id` ASC),
+  INDEX `fk_resource_has_language_resource1_idx` (`resource_id` ASC),
+  CONSTRAINT `fk_resource_has_language_resource1`
+    FOREIGN KEY (`resource_id`)
+    REFERENCES `resource` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_resource_has_language_language1`
+    FOREIGN KEY (`language_id`)
+    REFERENCES `language` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 SET SQL_MODE = '';
 DROP USER IF EXISTS codeuser@localhost;
 SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -369,7 +386,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `codingcoveragedb`;
-INSERT INTO `resource` (`id`, `link`, `description`, `date_added`, `difficulty`, `language_id`) VALUES (1, 'https://www.tutorialspoint.com/java/java_environment_setup.htm', 'basic tutorial', NULL, 'easy', 1);
+INSERT INTO `resource` (`id`, `link`, `description`, `date_added`, `difficulty`) VALUES (1, 'https://www.tutorialspoint.com/java/java_environment_setup.htm', 'basic tutorial', NULL, 'easy');
 
 COMMIT;
 
@@ -460,6 +477,17 @@ COMMIT;
 START TRANSACTION;
 USE `codingcoveragedb`;
 INSERT INTO `task_has_resource` (`task_id`, `resource_id`) VALUES (1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `resource_has_language`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `codingcoveragedb`;
+INSERT INTO `resource_has_language` (`resource_id`, `language_id`) VALUES (1, 1);
+INSERT INTO `resource_has_language` (`resource_id`, `language_id`) VALUES (1, 2);
 
 COMMIT;
 
