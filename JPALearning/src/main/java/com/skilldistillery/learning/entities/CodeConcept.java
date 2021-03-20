5596fec7a95f8bb378ed1f02f7cb3ea69303e53f
tylorshearn <tylorshearn95@gmail.com>
2021-03-20 18:56:38 -0400

    Complete CodeConcept-Task relationship

--- JPALearning/src/main/java/com/skilldistillery/learning/entities/CodeConcept.java
+++ JPALearning/src/main/java/com/skilldistillery/learning/entities/CodeConcept.java
@@ -1,11 +1,15 @@
 package com.skilldistillery.learning.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -24,11 +28,42 @@ public class CodeConcept {
 	@JoinColumn(name = "roadmap_id")
 	private Roadmap roadmap;
 	
+	@OneToMany(mappedBy="codeConcept")
+	private List<Task> task;
+	
 	
 	public CodeConcept() {
 		super();
 	}
+	
+	public CodeConcept(int id, String name, String description, Roadmap roadmap, List<Task> task) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.roadmap = roadmap;
+		this.task = task;
+	}
 
+	public void addTask(Task taskStub) {
+		if(task == null) task = new ArrayList<>();
+		
+		if (!task.contains(taskStub)) {
+			task.add(taskStub);
+			if (taskStub.getCodeConcept() != null) {
+				taskStub.getCodeConcept().getTask().remove(taskStub);
+			}
+			taskStub.setCodeConcept(this);
+		}
+	}
+	
+	public void removeTask(Task taskStub) {
+		taskStub.setCodeConcept(null);
+		if (task != null) {
+			task.remove(taskStub);
+		}
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -61,6 +96,14 @@ public class CodeConcept {
 		this.roadmap = roadmap;
 	}
 
+	public List<Task> getTask() {
+		return task;
+	}
+
+	public void setTask(List<Task> task) {
+		this.task = task;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -85,6 +128,8 @@ public class CodeConcept {
 
 	@Override
 	public String toString() {
-		return "CodeConcept [id=" + id + ", name=" + name + ", description=" + description + "]";
+		return "CodeConcept [id=" + id + ", name=" + name + ", description=" + description + ", roadmap=" + roadmap
+				+ ", task=" + task + "]";
 	}
+
 }
--- JPALearning/src/main/java/com/skilldistillery/learning/entities/Task.java
+++ JPALearning/src/main/java/com/skilldistillery/learning/entities/Task.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Task {
@@ -15,6 +17,10 @@ public class Task {
 	private String name;
 	
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="code_concept_id")
+	private CodeConcept codeConcept;
 
 	// TODO code_concept id CodeConcept Relation
 
@@ -55,15 +61,19 @@ public class Task {
 		this.description = description;
 	}
 
-	
-	
+	public CodeConcept getCodeConcept() {
+		return codeConcept;
+	}
+
+	public void setCodeConcept(CodeConcept codeConcept) {
+		this.codeConcept = codeConcept;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
 		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
 
@@ -76,29 +86,15 @@ public class Task {
 		if (getClass() != obj.getClass())
 			return false;
 		Task other = (Task) obj;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
 		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Task [id=").append(id).append(", name=").append(name).append(", description=")
-				.append(description).append("]");
-		return builder.toString();
+		return "Task [id=" + id + ", name=" + name + ", description=" + description + ", codeConcept=" + codeConcept
+				+ "]";
 	}
-	
-	
-	
+
 }
