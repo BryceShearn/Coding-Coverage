cba61a60c303f07246e43f1930f80dd7ec77ed2c
Thomas Webel <Thomaswebel@gmail.com>
2021-03-19 18:24:28 -0600

    Created Comment Entity, Tested Mapping

---
+++ JPALearning/src/main/java/com/skilldistillery/learning/entities/Comment.java
@@ -0,0 +1,104 @@
+package com.skilldistillery.learning.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+	
+	@Column(name="date_created")
+	private LocalDateTime dateCreated;
+
+	@Column(name="date_updated")
+	private LocalDateTime dateUpdated;
+	
+	//TODO post_id Post obj relation 
+	//TODO user_id User obj relation 
+	
+	public Comment() {
+		super();
+	}
+
+	public Comment(int id, String content, LocalDateTime dateCreated, LocalDateTime dateUpdated) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.dateCreated = dateCreated;
+		this.dateUpdated = dateUpdated;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(LocalDateTime dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public LocalDateTime getDateUpdated() {
+		return dateUpdated;
+	}
+
+	public void setDateUpdated(LocalDateTime dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Comment [id=").append(id).append(", content=").append(content).append(", dateCreated=")
+				.append(dateCreated).append(", dateUpdated=").append(dateUpdated).append("]");
+		return builder.toString();
+	}
+	
+	
+}
