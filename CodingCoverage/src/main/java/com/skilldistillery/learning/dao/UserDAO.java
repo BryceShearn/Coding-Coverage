45421fc922ade2eb5897a30e8aa5b9e7e8e61b7b
Thomas Webel <Thomaswebel@gmail.com>
2021-03-21 22:15:14 -0600

    updated User dao to inc. findAll / update / delete / archive user

--- CodingCoverage/src/main/java/com/skilldistillery/learning/dao/UserDAO.java
+++ CodingCoverage/src/main/java/com/skilldistillery/learning/dao/UserDAO.java
@@ -1,8 +1,22 @@
 package com.skilldistillery.learning.dao;
 
+import java.util.List;
+
 import com.skilldistillery.learning.entities.User;
 
 public interface UserDAO {
 	
 	User findById(int userId);
+	
+	List<User> findAll();
+	
+	User createUser(User newUser);
+	
+	User updateUser(User updatedUser);
+	
+	boolean deleteUser(User targetUser);
+	
+	boolean archiveUser(User targetUser);
+	
+	
 }
--- CodingCoverage/src/main/java/com/skilldistillery/learning/dao/UserDaoJpaImpl.java
+++ CodingCoverage/src/main/java/com/skilldistillery/learning/dao/UserDaoJpaImpl.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.learning.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -14,11 +16,58 @@ public class UserDaoJpaImpl implements UserDAO {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
+	// Get
 	@Override
 	public User findById(int userId) {
-		// TODO Auto-generated method stub
+
 		return em.find(User.class, userId);
+	}
+
+	@Override
+	public List<User> findAll() {
+		String getAllUsers = "SELECT u FROM User u";
+
+		List<User> allUsers = em.createQuery(getAllUsers, User.class).getResultList();
+
+		return allUsers;
+	}
+
+	// Create
+	@Override
+	public User createUser(User newUser) {
+		em.persist(newUser);
+		em.flush();
+
+		return newUser;
+	}
+
+	// Update
+	@Override
+	public User updateUser(User updatedUser) {
+
+		return em.merge(updatedUser);
+	}
+
+	// Deleted
+	@Override
+	public boolean deleteUser(User targetUser) {
+		boolean deleted = false;
+
+		if (targetUser != null) {
+			em.remove(targetUser);
+		}
+		deleted = !em.contains(targetUser);
+
+		return deleted;
+	}
+	
+	// archive 
+	@Override
+	public boolean archiveUser(User targetUser) {
+		targetUser.setEnabled(false);
+		em.merge(targetUser);
+		return false;
 	}
 
 }
