6428a9bd8511c438d1433f75a1f0da7e5ed59bff
Thomas Webel <Thomaswebel@gmail.com>
2021-03-21 20:06:01 -0600

    addition to sandbox queries

--- DB/ccsandbox.sql
+++ DB/ccsandbox.sql
@@ -20,14 +20,16 @@ SELECT * FROM post;
 SELECT * FROM post_vote;
 SELECT * FROM comment_vote;
 SELECT * FROM comment;
+SELECT * FROM language;
 SELECT * FROM task;
+SELECT * FROM roadmap;
 SELECT * FROM user_roadmap_task;
+SELECT * FROM resource;
+
+Select * from comment join user on user.id = comment.user_id where user.id = 2;
 
 
-
-
-
-
+SELECT * FROM post join post_vote on post.id = post_vote.post_id where post.id = 1;
 
 
 
