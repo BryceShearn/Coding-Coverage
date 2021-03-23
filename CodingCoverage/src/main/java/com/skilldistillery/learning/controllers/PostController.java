469296675186f8bd62db34fb472de9a1f3592b5a
tylorshearn <tylorshearn95@gmail.com>
2021-03-23 12:54:42 -0400

    Created Stub Post controller

--- CodingCoverage/src/main/java/com/skilldistillery/learning/controllers/PostController.java
+++ CodingCoverage/src/main/java/com/skilldistillery/learning/controllers/PostController.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.learning.controllers;
 
-public class PostController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import com.skilldistillery.learning.dao.PostDAO;
+
+@Controller
+public class PostController {
+	
+	@Autowired
+	PostDAO postDAO;
+	
 }
