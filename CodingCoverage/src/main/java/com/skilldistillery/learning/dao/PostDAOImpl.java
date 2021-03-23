6d75ecaa46d3a965a0938e8ccb117b294f729919
tylorshearn <tylorshearn95@gmail.com>
2021-03-23 13:52:33 -0400

    Added @Service/@Transactional to PostDAOImpl

--- CodingCoverage/src/main/java/com/skilldistillery/learning/dao/PostDAOImpl.java
+++ CodingCoverage/src/main/java/com/skilldistillery/learning/dao/PostDAOImpl.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
 
 import com.skilldistillery.learning.entities.Post;
 import com.skilldistillery.learning.entities.User;
 
+@Service
+@Transactional
 public class PostDAOImpl implements PostDAO {
 	
 	@PersistenceContext
