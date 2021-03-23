edc68d5d3bea4a883f81161c5451f2e945b6ebfa
tylorshearn <tylorshearn95@gmail.com>
2021-03-23 13:49:33 -0400

    PostController viewForum method stub

--- CodingCoverage/src/main/java/com/skilldistillery/learning/controllers/PostController.java
+++ CodingCoverage/src/main/java/com/skilldistillery/learning/controllers/PostController.java
@@ -2,6 +2,8 @@ package com.skilldistillery.learning.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skilldistillery.learning.dao.PostDAO;
 
@@ -10,5 +12,10 @@ public class PostController {
 	
 	@Autowired
 	PostDAO postDAO;
+	
+	@RequestMapping(path="")
+	public String viewForum(Model model) {
+		return "";
+	}
 	
 }
