edbc9a506291601d1264f442bf92673f524ff0ea
tylorshearn <tylorshearn95@gmail.com>
2021-03-24 02:30:32 -0400

    Stub

--- CodingCoverage/src/main/java/com/skilldistillery/learning/controllers/CommentController.java
+++ CodingCoverage/src/main/java/com/skilldistillery/learning/controllers/CommentController.java
@@ -2,11 +2,14 @@ package com.skilldistillery.learning.controllers;
 
 import java.time.LocalDateTime;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.skilldistillery.learning.dao.CommentDAO;
 import com.skilldistillery.learning.entities.Comment;
@@ -24,5 +27,11 @@ public class CommentController {
 		comment.setDateUpdated(LocalDateTime.now());
 		commentDao.createComment(comment);
 		return "forms/ViewingForum";
+	}
+	
+	@RequestMapping(path= "viewComment.do", params= "id")
+	public String viewComment(Model model, HttpSession session, @RequestParam("id")Integer commentId) {
+		model.addAttribute("comment", commentDao.findById(commentId));
+		return "results/ViewComment";
 	}
 }
