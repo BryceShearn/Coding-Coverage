d2571cc829b077eb83ee0b08065bdbdfb32ba6a3
Thomas Webel <Thomaswebel@gmail.com>
2021-03-24 09:24:58 -0600

    forced comment load with user.comments.size()

--- CodingCoverage/src/main/java/com/skilldistillery/learning/dao/AuthenticationDAOimpl.java
+++ CodingCoverage/src/main/java/com/skilldistillery/learning/dao/AuthenticationDAOimpl.java
@@ -37,6 +37,7 @@ public class AuthenticationDAOimpl implements AuthenticationDAO {
 		}catch (NoResultException e) {
 			return null;
 		}
+		foundUser.getComments().size();
 		
 		return foundUser;
 	}
